6980faa8a680c6a89a2407966bfe7f6e867c066f
Nehemias Eliseo Santos Carranza <neli92santscz@gmail.com>
2018-09-16 10:15:37 -0400

    rm-rf tickets and startapp tickets

--- backend/tickets/base/signals.py
+++
@@ -1,9 +0,0 @@
-import warnings
-from typing import Any, Callable, List, Tuple
-
-import django.dispatch
-from django.apps import apps
-from django.conf import settings
-from django.dispatch.dispatcher import NO_RECEIVERS
-
-from .models import Event
